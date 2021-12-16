773656625a8fc005ee990e4f2b2326083dbf374f
qefKH3up <BIT607Temp@gmail.com>
2021-12-17 12:19:24 +1300

    paintComponent() added.

--- Pong/PongPanel.java
+++ Pong/PongPanel.java
@@ -5,24 +5,30 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.Color;
 import javax.swing.Timer;
+// For Graphics
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 // This class will be handling the game logic along with anything related to the game, such as the score, ball, and paddles.
 // This class inherits from JPanel class, which allows program to draw things in the frame.
 // It also uses ActionListener and KeyListener libraries which allow program to respond to key presses and events.
 // You need to add unimplemented methods automatically via error message on PongPanel
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
-	
 	// Setting the background colour which will remain the same.
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	// Sets the time between calls.
 	private final static int TIMER_DELAY = 5;
-	
+
 	public PongPanel() {
 		// Import java.awt.Color for this.
 		setBackground(BACKGROUND_COLOUR);
-		 Timer timer = new Timer(TIMER_DELAY, this);
-         timer.start();
+		// Adding a timer object
+		// Updates the movement and positions of the objects and checks for a winner.
+		// Requires library javax.swing.Timer;
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
-	
+
 	// Changed default variable name arg0 to event for improved clarity.
 	@Override
 	public void keyPressed(KeyEvent event) {
@@ -39,8 +45,24 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		// TODO Auto-generated method stub
 	}
 
+	// actionPerformed method belonging to the ActionListener class.
+	// Overriding a method from a parent class, in this case ActionListener.
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		// Calls the update method.
+		update();
+	}
+
+	// Method which contains logic for each frame update of the game.
+	private void update() {
+	}
+
+	// paintComponent() method which paints graphics to the screen.
+	// Requires import java.awt.Graphics; and import java.awt.Graphics2D;
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
 	}
 }
